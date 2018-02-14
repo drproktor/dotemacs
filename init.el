37c6e0dface407b51805a33181a2e84ce7315afd
Max Huber <maxh@mailbox.org>
2018-02-14 23:32:00 +0100

    Optionally load local init

--- init.el
+++ init.el
@@ -366,4 +366,17 @@
 ;;-----------------------------------------------------------------------------
 ;; load local settings
 ;;-----------------------------------------------------------------------------
-(load-file "~/.emacs.d/init.local.el")
+(when (file-exists-p "~/.emacs.d/init.local.el")
+  (load-file "~/.emacs.d/init.local.el"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((TeX-master . "application") (TeX-master . t)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
